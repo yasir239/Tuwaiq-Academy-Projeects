973e2c70f45cd805a43aab69898de54f829888a0
yasir239 <yasoryhaaland@gmail.com>
2024-08-14 05:20:50 +0300

    Add files via upload

---
+++ Tuwaiq Academy project/project/app/urls.py
@@ -0,0 +1,37 @@
+from django.urls import  path
+from .views import *
+from .views import register_user, user_login, user_logout
+from django.urls import path, include
+from .views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('Admin', AdminViewSet, basename='Admin')
+router.register('Teacher', TeacherViewSet, basename='Teacher')
+router.register('Student', StudentViewSet, basename='Student')
+
+router.register('Lesson', LessonViewSet, basename='lesson')
+router.register('Assignment', AssignmentViewSet, basename='Assignment')
+
+router.register('Attendance', AttendanceViewSet, basename='Attendance')
+router.register('Grade', GradeViewSet, basename='Grade')
+
+urlpatterns = [
+  path('viewset/', include(router.urls)),
+  path('viewset/<int:pk>/', include(router.urls)),
+  path('register/', register_user, name='register'),
+  path('login/', user_login, name='login'),
+  path('logout/', user_logout, name='logout'),
+  path('change_password/', change_password, name='change_password'),
+
+
+
+
+]
+
+
+
+
+
+
+
---
+++ Tuwaiq Academy project/project/app/views.py
@@ -0,0 +1,141 @@
+from django.contrib.auth import authenticate
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from rest_framework import status, generics, mixins, viewsets
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .models import CustomUser,Admin, Lesson, Student, Teacher, Assignment, Attendance, Grade
+from .serializers import AdminSerializer,UserSerializer, ChangePasswordSerializer, LessonSerializer, StudentSerializer, TeacherSerializer, AssignmentSerializer, AttendanceSerializer, GradeSerializer
+
+
+class LessonViewSet(viewsets.ModelViewSet):
+
+
+   serializer_class = LessonSerializer
+   queryset = Lesson.objects.all()
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+
+
+   serializer_class = StudentSerializer
+   queryset = Student.objects.all()
+
+
+class TeacherViewSet(viewsets.ModelViewSet):
+    serializer_class = TeacherSerializer
+    queryset = Teacher.objects.all()
+
+
+class AssignmentViewSet(viewsets.ModelViewSet):
+
+   serializer_class = AssignmentSerializer
+   queryset = Assignment.objects.all()
+
+
+
+
+
+class AttendanceViewSet(viewsets.ModelViewSet):
+    serializer_class = AttendanceSerializer
+    queryset = Attendance.objects.all()
+
+
+class GradeViewSet(viewsets.ModelViewSet):
+    serializer_class = GradeSerializer
+    queryset = Grade.objects.all()
+
+class AdminViewSet(viewsets.ModelViewSet):
+    serializer_class = AdminSerializer
+    queryset = Admin.objects.all()
+
+
+@api_view(['POST'])
+def register_user(request):
+   if request.method == 'POST':
+       serializer = UserSerializer(data=request.data)
+       if serializer.is_valid():
+           serializer.save()
+           return Response(serializer.data, status=status.HTTP_201_CREATED)
+       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+
+@api_view(['POST'])
+def register_user(request):
+   if request.method == 'POST':
+       serializer = UserSerializer(data=request.data)
+       if serializer.is_valid():
+           serializer.save()
+           return Response(serializer.data, status=status.HTTP_201_CREATED)
+       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+@api_view(['POST'])
+def user_login(request):
+   if request.method == 'POST':
+       username = request.data.get('username')
+       password = request.data.get('password')
+
+
+       user = None
+       if '@' in username:
+           try:
+               user = CustomUser.objects.get(email=username)
+           except ObjectDoesNotExist:
+               pass
+
+
+       if not user:
+           user = authenticate(username=username, password=password)
+
+
+       if user:
+           token, _ = Token.objects.get_or_create(user=user)
+           return Response({'token': token.key}, status=status.HTTP_200_OK)
+
+
+       return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def user_logout(request):
+   if request.method == 'POST':
+       try:
+           # Delete the user's token to logout
+           request.user.auth_token.delete()
+           return Response({'message': 'Successfully logged out.'}, status=status.HTTP_200_OK)
+       except Exception as e:
+           return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def change_password(request):
+    if request.method == 'POST':
+        serializer = ChangePasswordSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            user = request.user
+            if not user.check_password(serializer.validated_data.get('old_password')):
+                return Response({'old_password': 'Wrong password.'}, status=status.HTTP_400_BAD_REQUEST)
+            user.set_password(serializer.validated_data.get('new_password'))
+            user.save()
+            return Response({'message': 'Password successfully changed.'}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({'error': 'Invalid request method.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
